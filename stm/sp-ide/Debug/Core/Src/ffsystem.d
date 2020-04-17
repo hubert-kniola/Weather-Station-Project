03be8988b20eb7bc8fb8030ea39193526fe2c538
bart-kosmala <bartoshmen@gmail.com>
2020-04-17 10:18:19 +0600

    clean2

--- stm/sp-ide/Debug/Core/Src/ffsystem.d
+++
@@ -1,6 +0,0 @@
-Core/Src/ffsystem.o: ../Core/Src/ffsystem.c ../Core/Inc/ff.h \
- ../Core/Inc/ffconf.h
-
-../Core/Inc/ff.h:
-
-../Core/Inc/ffconf.h:
--- stm/sp-ide/Debug/Core/Src/ffunicode.d
+++
@@ -1,6 +0,0 @@
-Core/Src/ffunicode.o: ../Core/Src/ffunicode.c ../Core/Inc/ff.h \
- ../Core/Inc/ffconf.h
-
-../Core/Inc/ff.h:
-
-../Core/Inc/ffconf.h:
